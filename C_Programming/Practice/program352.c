b8e09a45d42bb56d5dc8f671c4e59315a37921ae
Unmesh Singnurkar <unmeshsingnurkar@gmail.com>
2026-08-29 20:37:50 +0530

    Add files via upload

---
+++ C_Programming/Practice/program352.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+#pragma pack (1)
+
+struct node
+{
+    int data;
+    struct node * next;
+};
+
+
+int main()
+{
+    struct node obj;
+
+    printf("%lu\n",sizeof(obj));        // 12 bytes
+    // 4 + 8
+    // 4(int) + 8(pointer)
+
+    return 0;
+}
